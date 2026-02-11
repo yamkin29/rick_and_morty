9460617831694b77028b52d51e0834d98ef86532
Aleksey Yamkin <me@jamkin.ru>
2026-02-11 07:09:21 +0300

    Реализовал свой ClassNames.ts

---
+++ src/shared/helpers/ClassNames.ts
@@ -0,0 +1,24 @@
+export type ClassNamesArg = string | undefined | Record<string, boolean>;
+
+const ClassNames = (...args: ClassNamesArg[]) => {
+  const result = [];
+
+  for (const arg of args) {
+    if (arg) {
+      if (typeof arg === 'string') {
+        result.push(arg);
+      }
+      if (typeof arg === 'object') {
+        for (const key in arg) {
+          if (arg[key]) {
+            result.push(key);
+          }
+        }
+      }
+    }
+  }
+
+  return result.join(' ');
+};
+
+export default ClassNames;
---
+++ src/shared/helpers/index.ts
@@ -0,0 +1,1 @@
+export { default as ClassNames } from './ClassNames.ts';
