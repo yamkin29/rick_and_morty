376d8f44652424ac240a1f2bd22573342c7b91bf
Aleksey Yamkin <me@jamkin.ru>
2026-02-19 09:09:10 +0300

    Реализовал режим редактирования для карточки персонажа

--- src/pages/charactersListPage/CharactersListPage.tsx
+++ src/pages/charactersListPage/CharactersListPage.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
-
 import { MainIcon } from '@/assets';
-import { TestImageRick } from "@/assets";
+import { TestImageRick } from '@/assets';
+import { CharacterCardWidget } from '@/widgets';
 
 import './CharactersListPage.css';
-import {CharacterCardWidget} from "@/widgets";
 
 export const CharactersListPage = () => {
-  const [value, setValue] = useState<string>('');
-
-  const handleChange = (newValue: string) => setValue(newValue);
-
   return (
     <div className='characters-list-page'>
       <div className='characters-list-page__logo'>
         <MainIcon />
       </div>
       <div className='characters-list-page__list'>
-        <CharacterCardWidget mode='view' image={<TestImageRick/>}/>
+        <CharacterCardWidget
+          mode='edit'
+          image={<TestImageRick />}
+        />
       </div>
     </div>
   );
--- src/widgets/characterCardWidget/CharacterCardWidget.tsx
+++ src/widgets/characterCardWidget/CharacterCardWidget.tsx
@@ -1,53 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Input, Select } from '@/shared/components';
 import { ClassNames } from '@/shared/helpers';
+
 import './CharacterCardWidget.css';
-import {Select} from "@/shared/components";
+
+type StatusVariants = 'alive' | 'dead' | 'unknown';
+type Option = { label: string; value: StatusVariants };
 
 interface ICharacterCardWidgetProps {
+  characterId?: string;
   mode: 'view' | 'edit';
   image: React.ReactNode;
 }
 
-export const CharacterCardWidget = ({ mode, image }: ICharacterCardWidgetProps) => {
+const options: Option[] = [
+  { label: 'Alive', value: 'alive' },
+  { label: 'Dead', value: 'dead' },
+  { label: 'Unknown', value: 'unknown' }
+];
+
+const StatusOption = ({ option }: { option: Option }) => {
+  return (
+    <div className={ClassNames('character-card__status-option')}>
+      <div>{option.label}</div>
+      <div className={ClassNames('character-card__status-dot', `character-card__status-dot--${option.value}`)}></div>
+    </div>
+  );
+};
+
+export const CharacterCardWidget = ({ characterId, mode, image }: ICharacterCardWidgetProps) => {
+  const [selectValue, setSelectValue] = useState<StatusVariants | null>('alive');
+  const [nameValue, setNameValue] = useState<string>('Rick Sanchez');
+  const [locationValue, setLocationValue] = useState<string>('Earth');
+
+  const currentOption = options.find((option) => option.value === selectValue) ?? options[0];
+
+  const nameInputId = `character-${characterId}-name`;
+  const locationInputId = `character-${characterId}-location`;
+
+  const handleSelectClick = (newSelectValue: StatusVariants | null) => {
+    setSelectValue(newSelectValue);
+  };
+
+  const handleInputNameClick = (newNameValue: string) => {
+    setNameValue(newNameValue);
+  };
+
+  const handleSelectLocationClick = (newLocationValue: string) => {
+    setLocationValue(newLocationValue);
+  };
+
   return (
     <div className={ClassNames('character-card', `character-card--${mode}`)}>
-      <div className={ClassNames('character-card__media')}>
-        { image }
-      </div>
+      <div className={ClassNames('character-card__media')}>{image}</div>
       <div className={ClassNames('character-card__content')}>
         <div className={ClassNames('character-card__title')}>
-          Rick Sanchez
+          {mode === 'view' ? (
+            nameValue
+          ) : (
+            <Input
+              value={nameValue}
+              id={nameInputId}
+              onChange={handleInputNameClick}
+              variant='underlined'
+            />
+          )}
         </div>
         <div className={ClassNames('character-card__meta-item')}>
-          <div className={ClassNames('character-card__meta-label')}>
-            Gender
-          </div>
-          <div className={ClassNames('character-card__meta-value')}>
-            Male
-          </div>
+          <div className={ClassNames('character-card__meta-label')}>Gender</div>
+          <div className={ClassNames('character-card__meta-value')}>Male</div>
         </div>
         <div className={ClassNames('character-card__meta-item')}>
-          <div className={ClassNames('character-card__meta-label')}>
-            Species
-          </div>
-          <div className={ClassNames('character-card__meta-value')}>
-            Human
-          </div>
+          <div className={ClassNames('character-card__meta-label')}>Species</div>
+          <div className={ClassNames('character-card__meta-value')}>Human</div>
         </div>
         <div className={ClassNames('character-card__meta-item')}>
-          <div className={ClassNames('character-card__meta-label')}>
-            Location
-          </div>
+          <div className={ClassNames('character-card__meta-label')}>Location</div>
           <div className={ClassNames('character-card__meta-value')}>
-            Earth
+            {mode === 'view' ? (
+              locationValue
+            ) : (
+              <Input
+                value={locationValue}
+                id={locationInputId}
+                onChange={handleSelectLocationClick}
+                variant='underlined'
+                className={ClassNames('character-card__location')}
+              />
+            )}
           </div>
         </div>
         <div className={ClassNames('character-card__status')}>
-          <div className={ClassNames('character-card__meta-label')}>
-            Status
-          </div>
+          <div className={ClassNames('character-card__meta-label')}>Status</div>
           <div className={ClassNames('character-card__status-value')}>
-            {/*<Select options={} value={} onChange={}/>*/}
+            {mode === 'view' ? (
+              <StatusOption option={currentOption} />
+            ) : (
+              <Select
+                options={options}
+                value={selectValue}
+                onChange={handleSelectClick}
+                size='small'
+                OptionComponent={StatusOption}
+              />
+            )}
           </div>
         </div>
       </div>
