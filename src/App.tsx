c2dc8a6a885408c45c2ffccd721fc83ba818a84c
Aleksey Yamkin <me@jamkin.ru>
2026-03-18 16:45:30 +0300

    Внес правки по код ревью

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Navigate, Route, Routes } from 'react-router';
 
 import { CharacterPage, CharactersListPage, NotFoundPage } from '@/pages';
 import { Footer, Header } from '@/shared/components';
+import { ErrorBoundary } from '@/shared/components';
 import { AppToaster } from '@/shared/components/appToaster';
-import { ErrorBoundary } from '@/shared/components/errorBoundary/ErrorBoundary';
 
 import './App.scss';
 
