785d7092bba938c19ad33419924890c9a3b835a2
Aleksey Yamkin <me@jamkin.ru>
2026-03-29 17:44:33 +0300

    Подготовка к деплою

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, HashRouter } from 'react-router';
 
 import { App } from './App.tsx';
 
 import './index.scss';
 
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
