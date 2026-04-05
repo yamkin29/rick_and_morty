1c3342ad2f78b55c59ce0a4b4731b186ba575ab2
Aleksey Yamkin <me@jamkin.ru>
2026-04-05 18:55:29 +0300

    Добавил в проект анализатор бандла vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import { analyzer } from 'vite-bundle-analyzer';
 import svgr from 'vite-plugin-svgr';
 
 const repositoryName = 'rick_and_morty';
 const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true';
+const isAnalyze = process.env.ANALYZE === 'true';
 
 export default defineConfig({
   base: isGitHubPagesBuild ? `/${repositoryName}/` : '/',
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    isAnalyze &&
+      analyzer({
+        analyzerMode: 'server',
+        openAnalyzer: false
+      })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
