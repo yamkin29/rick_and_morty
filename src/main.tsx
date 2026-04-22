5b2e8ef7854880c93d5fabd5b69f09360c0ff404
Aleksey Yamkin <me@jamkin.ru>
2026-04-22 09:30:09 +0300

    Правки по ревью

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, HashRouter } from 'react-router';
 
+import { AppToaster } from '@/shared/components/appToaster';
+
 import { App } from './App.tsx';
 
 import './index.scss';
@@ -14,8 +16,8 @@ const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 10,
+      staleTime: 1000 * 60 * 5, // 5 минут
+      gcTime: 1000 * 60 * 10, // 10 минут
       retry: 3
     }
   }
@@ -25,6 +27,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <AppToaster />
       <Router>
         <App />
       </Router>
